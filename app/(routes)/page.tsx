efc7898b4115e66d8ec9f1cd465174082ef7241b
Snow flowers <zero2392001@gmail.com>
2024-01-12 19:01:46 +0700

    Resize the billboards image

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,17 +7,19 @@ import React from 'react';
 export const revalidate = 0;
 const HomePage = async () => {
     const products = await getProducts({ isFeatured: true });
-    const billboards = await getBillboards('7d2172ca-299c-418d-b3ce-6b41f1ee5706');
+    const billboards = await getBillboards('29dbab6a-941f-4e69-8155-ca6ab22ee1f6');
 
     return (
-        <Container>
-            <div className="space-y-10 pb-10">
-                <Billboard data={billboards} />
-                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products} />
+        <>
+            <Billboard data={billboards} />
+            <Container>
+                <div className="space-y-10 pb-10">
+                    <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                        <ProductList title="Featured Products" items={products} />
+                    </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </>
     );
 };
 
